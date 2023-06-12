d6fbc5bfadce7ccd425e12a5ccdfb88ed4601f9c
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-06-12 22:57:25 +0800

    Add files via upload

---
+++ frontend/src/components/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import MyNavBar from "../mynavbar/MyNavBar";
+
+export default function Homepage() {
+  return (
+    <>
+      <MyNavBar />
+      <div className="homepageWrapper">
+        <h1>Hello World!</h1>
+      </div>
+    </>
+  );
+}
