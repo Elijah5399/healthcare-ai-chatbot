76847b2df59c7bde0e6e0b74ecef9c6741371ec0
Elijah5399 <chia.elijah@gmail.com>
2023-06-13 00:22:29 +0800

    Testing out gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require("express");
 
 /* Creating Express App */
-const expressApp = express()
+const expressApp = express();
 
 /* Reacting to Requests */
-expressApp.length('/', (request, response) => {
-    response.json({msg: 'Welcome to the app'})
-})
+expressApp.length("/", (request, response) => {
+  response.json({ msg: "Welcome to the app" });
+});
 
 /* Listen for Requests */
 expressApp.listen(4000, () => {
-    console.log('listening on port 4000')
-})
+  console.log("listening on port 4000");
+});
