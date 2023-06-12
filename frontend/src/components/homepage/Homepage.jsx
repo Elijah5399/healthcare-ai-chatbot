2520e680ad1c96eff6807238c5fdf26da8c6d433
Goh Teng Fong <gohtengfong@Gohs-MacBook-Pro-4.local>
2023-06-13 00:47:32 +0800

    started on navbar and homepage

--- frontend/src/components/homepage/Homepage.jsx
+++ frontend/src/components/homepage/Homepage.jsx
@@ -1,11 +1,52 @@
+import '../../styles/Homepage.css';
 import MyNavBar from "../mynavbar/MyNavBar";
+import ChatbotLogo from '../.././chatbot-logo.png';
 
 export default function Homepage() {
   return (
     <>
       <MyNavBar />
       <div className="homepageWrapper">
-        <h1>Hello World!</h1>
+        <h1>Ask Me Anything!</h1>
+
+        <body>
+          {/* Main Container */}
+          <div className="homepageContainer">
+          
+          {/* Message Header Section */}
+            <div className="msg-header">
+              <div class="container1">
+                <img src={ChatbotLogo} class="msgimg"/>
+                <div class="active">
+                  <p>ChatBot @ SGH</p>
+                </div>
+              </div>
+            </div>
+
+          {/* Chat Inbox Section */}
+          <div className="chat-page"> 
+            <div className="msg-inbox">
+              <div className="chats">
+                <div className="msg-page">
+          {/* Contains Incoming & Outgoing Messages */}   
+                </div>
+              </div>
+            
+          {/* Message Bottom Section */}
+              <div className="msg-bottom">
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Write message..." />
+                    <div class="input-group-append ">
+                      <span class="input-group-text send-icon "><i class="bi bi-send "></i></span>
+                    </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        </body>
+
       </div>
     </>
   );
--- frontend/src/components/mynavbar/MyNavBar.jsx
+++ frontend/src/components/mynavbar/MyNavBar.jsx
@@ -2,13 +2,14 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import SGHLogo from '../.././SGH-logo.png'
+import SGHLogo from '../.././SGH-logo.png';
+import "../../styles/MyNavBar.css";
 
 function MyNavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <img src={SGHLogo} />
+        <img className="sghlogo" src={SGHLogo} />
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
