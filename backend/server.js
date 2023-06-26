12e9e215cf7ce758002b2e156b096db157484308
Goh Teng Fong <gohtengfong@Gohs-MacBook-Pro-4.local>
2023-06-26 17:27:22 +0800

    connected to db, added models and schemas, basic routes

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,37 @@
 const express = require("express");
-require("dotenv").config();
+const mongoose = require("mongoose");
 const executeQueries = require("./dialogflow");
+require("dotenv").config();
+const User = require("./models/UsersModel");
+const Appointment = require("./models/AppointmentsModel");
 
 /* Creating Express App */
 const expressApp = express();
 
+/* Connecting to DB */
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // connect to database first
+        .then(() => expressApp.listen(3000, () => { // begin listening for requests second
+          console.log("connected to db and listening on port 3000 ")}))
+        .catch((error) => console.log(error))
+
 /* Middleware (???) */
 expressApp.use(express.json());
-
 expressApp.use(express.static("../frontend/index.js"));
-/*
-expressApp.use((request, response, next) => {
-  console.log(request.path, request.method);
-  next();
-});
-*/
 
 /* Reacting to Requests */
+expressApp.get("/", (request, response) => { // when user goes to route "/" --> it fires a response to send back frontend (home page)
+  response.sendFile("../../frontend/src/index.js", { root: __dirname })
+});
 
-expressApp.get("/", (request, response) => {
-  // request
-  response.send("hello");
+expressApp.get("/add-appt", (request, response) => {
+  const appt = new Appointment({
+    id: "1",
+    date: "2023-06-26T14:30:00"  
+  })
+
+  appt.save()
+      .then((result) => response.send(result))
+      .catch((error) => console.log(error))
 });
 
 expressApp.get("/askQn", (req, res) => {
@@ -32,11 +43,6 @@ expressApp.get("/askQn", (req, res) => {
   ).then((ans) => res.send(ans));
 });
 
-/* Listen for Requests */
-expressApp.listen(4000, () => {
-  console.log("listening on port 4000");
-});
-
 /* Start */
-const usersRoutes = require("./routes/Users"); // obtaining the route
+const usersRoutes = require("./routes/Bookings"); // obtaining the route
 expressApp.use("/api/users", usersRoutes); // using the route only when the url consists of /api/users
