12656861ec3deb94f26c69b7d43f3d0a4b2d8c2c
Elijah5399 <chia.elijah@gmail.com>
2023-06-29 15:08:49 +0800

    .

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,35 +1,44 @@
-const express = require("express")
-const User = require("../models/UsersModel")
+const express = require("express");
+const User = require("../models/UsersModel");
+const jwt = require("jsonwebtoken");
 
-const router = express.Router()
+const router = express.Router();
+
+/* Function to Generate JWT */
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" }); // SECRET is a variable only known to the backend server to ensure greater security
+};
 
 /* Login */
 router.post("/login", async (request, response) => {
-    const {email, password} = request.body
-    
-    try {
-        const user = await User.login(email, password)
+  const { email, password } = request.body;
 
-        /* JSON Web Token */
-        // token code here!
+  try {
+    const user = await User.login(email, password);
 
-        response.status(200).json({user, token})
-    } catch (error) {
-        response.status(400).json({error: error.message})
-    }
-})
+    /* JSON Web Token */
+    // token code here!
+
+    response.status(200).json({ user, token });
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+});
 
 /* Signup */
 router.post("/signup", async (request, response) => {
-    const {name, email, password} = request.body
+  const { name, email, password } = request.body;
 
-    try {
-        const user = await User.signup(name, email, password) // .signup() is a statically created function in UsersModel.js
-        response.status(200).json({email, user})
-    } catch (error) {
-        response.status(400).json({error: error.message})
-    }
-})
+  try {
+    const user = await User.signup(name, email, password); // .signup() is a statically created function in UsersModel.js
 
+    /* JSON Web Token */
+    const token = createToken(user._id);
 
-module.exports = router
+    response.status(200).json({ email, user });
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = router;
