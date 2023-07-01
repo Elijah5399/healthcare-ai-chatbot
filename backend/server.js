b8202888818916fb14d525be3d3c7071c89dd5b1
Goh Teng Fong <gohtengfong@Gohs-MacBook-Pro-4.local>
2023-07-01 16:32:44 +0800

    cancel page will always be updated with data in database

--- backend/server.js
+++ backend/server.js
@@ -29,35 +29,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
           console.log("connected to db and listening on port 3001")}))
         .catch((error) => console.log(error))
 
-/* Reacting to Requests */
-expressApp.get("/", (request, response) => {
-  // when user goes to route "/" --> it fires a response to send back frontend (home page)
-  response.json({ mssg: "welcome!" });
-  // response.sendFile("../../frontend/src/index.js", { root: __dirname })
-});
-
-expressApp.get("/book/submit", async (request, response) => {
-  const { id, dateTime } = request.body;
-
-  try {
-    const appt = await apptModel.create({ id, dateTime });
-    response.status(200).json(appt);
-  } catch (error) {
-    response.status(400).json({ error: error.message });
-  }
-});
-
-// expressApp.get("/add-appt", (request, response) => {
-//   const appt = new Appointment({
-//     id: "1",
-//     date: "2023-06-26T14:30:00"
-//   })
-
-//   appt.save()
-//       .then((result) => response.send(result))
-//       .catch((error) => console.log(error))
-// });
-
+/* Elijah's Nonsense */
 expressApp.get("/askQn", (req, res) => {
   executeQueries(
     process.env.PROJECT_ID,
@@ -66,7 +38,3 @@ expressApp.get("/askQn", (req, res) => {
     "en"
   ).then((ans) => res.send(ans));
 });
-
-/* Start */
-const usersRoutes = require("./routes/BookingRoutes"); // obtaining the route
-expressApp.use("/api/users", usersRoutes); // using the route only when the url consists of /api/users
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AppointmentsContextProvider } from "./context/AppointmentsContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppointmentsContextProvider>
+      <App />
+    </AppointmentsContextProvider>
   </React.StrictMode>
 );
